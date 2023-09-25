f799d9473c04c0e3ccf9f53eda1f42c3884aefb9
Ibrahim Michael <segunmichael24@gmail.com>
2023-09-25 19:46:51 +0100

    Chapter 13 django4professionals

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -151,6 +151,9 @@ STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 AUTH_USER_MODEL = 'users.CustomUser'
 
 DEFAULT_FROM_EMAIL = 'admin@djangobookstore.com'
